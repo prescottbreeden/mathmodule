7404bfc5df86ae02ba44717c02532efcdde453d9
Prescott Breeden <prescott.breeden@gmail.com>
2018-04-29 14:45:18 -0700

    snore

---
+++ app.js
@@ -0,0 +1,13 @@
+var mathlib = require('./mathlib');
+
+const test1 = mathlib.add(1,2);
+console.log(test1);
+
+const test2 = mathlib.multiply(122,555);
+console.log(test2);
+
+const test3 = mathlib.square(123456);
+console.log(test3);
+
+const test4 = mathlib.random(1,5);
+console.log(test4);
